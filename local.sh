35a2ae27c17ee2dc041a4cd498a76d416b1f88fa
Doody Parizada <doody.parizada@gmail.com>
2019-03-26 16:43:08 +0200

    remove STELLAR_INITIAL_XLM_AMOUNT

--- local.sh
+++ local.sh
@@ -4,7 +4,6 @@ export \
     STELLAR_HORIZON_URL='https://horizon-testnet.kininfrastructure.com' \
     STELLAR_NETWORK='Kin Testnet ; December 2018' \
     KIN_FRIENDBOT='http://friendbot-testnet.kininfrastructure.com/' \
-    STELLAR_INITIAL_XLM_AMOUNT=2 \
     CHANNEL_SALT='some_salt' \
     APP_DEBUG=true \
     MAX_CHANNELS=1
--- payment/config.py
+++ payment/config.py
@@ -8,9 +8,8 @@ STELLAR_NETWORK = os.environ['STELLAR_NETWORK']
 STELLAR_ENV = Environment('CUSTOM', STELLAR_HORIZON_URL, STELLAR_NETWORK)
 
 CHANNEL_SALT = os.environ.get('CHANNEL_SALT')
-MAX_CHANNELS = int(os.environ.get('MAX_CHANNELS', '20'))
+MAX_CHANNELS = int(os.environ.get('MAX_CHANNELS', '1200'))
 STELLAR_BASE_SEED = os.environ['STELLAR_BASE_SEED']
-STELLAR_INITIAL_XLM_AMOUNT = float(os.environ['STELLAR_INITIAL_XLM_AMOUNT'])
 
 REDIS = os.environ['APP_REDIS']
 APP_NAME = os.environ.get('APP_NAME', 'payment-service')
