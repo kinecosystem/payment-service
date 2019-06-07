c62e2b74d3aa345568ea5f7319b3553732592c7e
Aryeh Katz <in@akatz.io>
2019-06-07 21:56:14 +0300

    add ongoing changes

--- local.sh
+++ local.sh
@@ -1,6 +1,4 @@
 export \
-    APP_REDIS='redis://localhost:6379/0' \
-    APP_PORT=5000 \
     STELLAR_HORIZON_URL='https://horizon-testnet.kininfrastructure.com' \
     STELLAR_NETWORK='Kin Testnet ; December 2018' \
     KIN_FRIENDBOT='http://friendbot-testnet.kininfrastructure.com/' \
